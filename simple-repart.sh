6f5ab8297fbc00b5679a00acf8a448169ce5cf1a
zombah <zombahatgmaildotcom>
2012-11-07 19:13:30 +0400

    Change to use variables and select
    Signed-off-by: zombah <zombahatgmaildotcom>

--- simple-repart.sh
+++ simple-repart.sh
@@ -14,56 +14,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Function first
-function quit { exit }
+last_partition_8gb=2876677 
+last_partition_16gb=14628096 
+last_partition_size=0 # Detect later
 
-function backup-8gb {
-sudo ./nvflash --bl bootloader.bin --rawdeviceread 0 1536 ac100-2.img --rawdeviceread 1536 256 ac100-3.img --rawdeviceread 1792 1024 ac100-4.img --rawdeviceread 2816 2560 ac100-5.img --rawdeviceread 5376 4096 ac100-6.img --rawdeviceread 9984 153600 ac100-8.img --rawdeviceread 163584 204800 ac100-9.img --rawdeviceread 368384 1024 ac100-10.img --rawdeviceread 369664 632320 ac100-12.img --rawdeviceread 1002240 2876677 ac100-14.img --go
+config_8gb="android-8gb.cfg"
+config_16gb="android-32gb.cfg"
+config="none" # Detect later
+
+
+function error {
+	echo -e "\n\e[00;31m$1\e[00m\n"
+	exit 3
 }
 
-function backup-32gb {
-sudo ./nvflash --bl bootloader.bin --rawdeviceread 0 1536 ac100-2.img --rawdeviceread 1536 256 ac100-3.img --rawdeviceread 1792 1024 ac100-4.img --rawdeviceread 2816 2560 ac100-5.img --rawdeviceread 5376 4096 ac100-6.img --rawdeviceread 9984 153600 ac100-8.img --rawdeviceread 163584 204800 ac100-9.img --rawdeviceread 368384 1024 ac100-10.img --rawdeviceread 369664 632320 ac100-12.img --rawdeviceread 1002240 14628096 ac100-14.img
+
+function backup {
+	sudo ./nvflash --bl bootloader.bin --rawdeviceread 0 1536 ac100-2.img --rawdeviceread 1536 256 ac100-3.img --rawdeviceread 1792 1024 ac100-4.img --rawdeviceread 2816 2560 ac100-5.img --rawdeviceread 5376 4096 ac100-6.img --rawdeviceread 9984 153600 ac100-8.img --rawdeviceread 163584 204800 ac100-9.img --rawdeviceread 368384 1024 ac100-10.img --rawdeviceread 369664 632320 ac100-12.img --rawdeviceread 1002240 ${last_partition_size} ac100-14.img --go
+	[[ $? == 0 ]] || error "Can't backup your ac100"
 }
+
 
 function create-bct {
 	echo "
 Press any key to continue to bct creation"
 	read -n 1 any
 
-dd if=ac100-2.img of=ac100.bct bs=4080 count=1 
+	dd if=ac100-2.img of=ac100.bct bs=4080 count=1 
 }
 
-function repart-8gb {
+
+function repart {
 	echo "
 Press any key to continue to repartition phase"
 	read -n 1 any
 
-sudo ./nvflash -r --bct ac100.bct --setbct --configfile android-8gb.cfg --create --verifypart -1 --go
+	sudo ./nvflash -r --bct ac100.bct --setbct --configfile "${config}" --create --verifypart -1 --go
+	[[ $? == 0 ]] || error "Can't repart your ac100"
 }
 
-function repart-32gb {
-	echo "
-Press any key to continue to repartition phase"
-	read -n 1 any
 
-sudo ./nvflash -r --bct ac100.bct --setbct --configfile android-32gb.cfg --create --verifypart -1 --go
-}
-
-function restore-8gb {
+function restore {
 	echo "
 Press any key to continue to flash phase"
 	read -n 1 any
 
-sudo ./nvflash -r --rawdevicewrite 0 1536 ac100-2.img --rawdevicewrite 1536 256 ac100-3.img --rawdevicewrite 1792 1024 ac100-4.img --sync
+	sudo ./nvflash -r --rawdevicewrite 0 1536 ac100-2.img --rawdevicewrite 1536 256 ac100-3.img --rawdevicewrite 1792 1024 ac100-4.img --sync
+	[[ $? == 0 ]] || error "Can't restore your ac100"
 }
 
-function restore-32gb {
-	echo "
-Press any key to continue to flash phase"
-	read -n 1 any
-
-sudo ./nvflash -r --rawdevicewrite 0 1536 ac100-2.img --rawdevicewrite 1536 256 ac100-3.img --rawdevicewrite 1792 1024 ac100-4.img --sync
-}
 
 # Main Script
 
@@ -79,8 +78,13 @@ REQUIREMENTS:
 "
 echo "Are you ready to continue? Press y or n:"
 read -n 1 ready
+echo -e "\n"
 
-if [ "$ready" == "y" ]; then
+if [ "$ready" == "n" ]; then
+ echo -e "Come back then you are ready."
+ exit 1
+fi
+
 clear
 
 echo "
@@ -88,38 +92,29 @@ What flash size of your ac100:
 Press 1 if 8GB
 Press 2 if 32GB"
 read -n 1 version
+echo -e "\n"
 
-    if [ "$version" == "1" ]; then 
+case $version in
+	"1")
+		last_partition_size="${last_partition_8gb}"
+		config="${config_8gb}"
+	;;
 
-	backup-8gb
-	create-bct
-	repart-8gb
-	restore-8gb
+	"2")
+		last_partition_size="${last_partition_32gb}"
+		config="${config_32gb}"
+	;;
 
-echo "
-Repartition finished. Reboot to verify."
-quit
+	*)
+		echo "Check your ac100 flash size"
+		exit 2
+	;;
+esac
 
-    elif [ "$version" == "2" ]; then
+backup
+create-bct
+repart
+restore
 
-	backup-32gb
-	create-bct
-	repart-32gb
-	restore-32gb
-
-echo "
-Repartition finished. Reboot to verify."
-quit
-
-    else
-     echo "
-     Check your ac100 flash size"
-     exit 2
-     fi
-
-else
- echo "
- Come back then you are ready."
- exit 1
-fi
+echo "Repartition finished. Reboot to verify."
 
